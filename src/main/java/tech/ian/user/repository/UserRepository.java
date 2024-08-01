4ae20819f023e455530661701b628e7d190f2888
Ian Leme <ianlemee@hotmail.com>
2024-08-01 18:33:18 -0300

    feat: criando repositorio

---
+++ src/main/java/tech/ian/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package tech.ian.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import tech.ian.user.entity.User;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    UserDetails findByEmail(String email);
+}
