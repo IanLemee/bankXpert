5e42abfe66d963c5ddc1593ad28777097930a1c5
Ian Leme <ianlemee@hotmail.com>
2024-08-13 11:27:56 -0300

    feat: adicionando metodo findUserEntityByEmail

--- src/main/java/tech/ian/user/repository/UserRepository.java
+++ src/main/java/tech/ian/user/repository/UserRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import tech.ian.user.entity.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     UserDetails findByEmail(String email);
+    User findUserEntityByEmail(String email);
 }
