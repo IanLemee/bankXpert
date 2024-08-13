64c16ca0ca911f84686ed70f050f75fe1281444d
Ian Leme <ianlemee@hotmail.com>
2024-08-13 11:27:25 -0300

    feat: adicionando logica account

--- src/main/java/tech/ian/user/service/UserService.java
+++ src/main/java/tech/ian/user/service/UserService.java
@@ -3,9 +3,15 @@ package tech.ian.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tech.ian.user.dto.CreateUserResponse;
+import tech.ian.user.entity.Account;
 import tech.ian.user.entity.User;
+import tech.ian.user.producers.UserProducer;
+import tech.ian.user.repository.AccountRepository;
 import tech.ian.user.repository.UserRepository;
+
+import java.math.BigDecimal;
 
 @Service
 public class UserService {
@@ -14,8 +20,15 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private UserProducer userProducer;
+
+    @Transactional
     public CreateUserResponse registerUser(User user) {
         if (userRepository.findByEmail(user.getEmail()) != null) {
             throw new RuntimeException("Esse email ja existe");
@@ -23,6 +36,14 @@ public class UserService {
             String encodedPassword = passwordEncoder.encode(user.getPassword());
             user.setPassword(encodedPassword);
 
+            Account account = new Account();
+            account.setUser(user);
+            account.setEmail(user.getEmail());
+            account.setBalance(BigDecimal.ZERO);
+            account.setDocument(user.getDocument());
+            accountRepository.save(account);
+
+            user.setAccount(account);
             User userSaved = userRepository.save(user);
 
             CreateUserResponse userResponse = new CreateUserResponse(
@@ -32,6 +53,7 @@ public class UserService {
                     userSaved.getPassword(),
                     userSaved.getDocument());
 
+            userProducer.publishMessageEmail(userResponse);
             return userResponse;
         }
     }
