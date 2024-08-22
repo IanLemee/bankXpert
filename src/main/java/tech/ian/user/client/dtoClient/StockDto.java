407cd6b76e970ad373ea1ca9cca8700c048a43b6
Ian Leme <ianlemee@hotmail.com>
2024-08-22 17:12:06 -0300

    feat: adicionando logica a camada de acoes

--- src/main/java/tech/ian/user/client/dtoClient/StockDto.java
+++ src/main/java/tech/ian/user/client/dtoClient/StockDto.java
@@ -2,5 +2,5 @@ package tech.ian.user.client.dtoClient;
 
 import java.math.BigDecimal;
 
-public record StockDto(BigDecimal regularMarketDayHigh, String currency, String symbol, String logourl) {
+public record StockDto(BigDecimal regularMarketPrice, String currency, String symbol, String logourl) {
 }
--- src/main/java/tech/ian/user/service/StockService.java
+++ src/main/java/tech/ian/user/service/StockService.java
@@ -2,15 +2,15 @@ package tech.ian.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import tech.ian.user.client.BrapiClient;
 import tech.ian.user.client.dtoClient.ResponseBrapiDto;
-import tech.ian.user.entity.Account;
-import tech.ian.user.entity.AccountInvestment;
-import tech.ian.user.entity.TransactionalAccount;
+import tech.ian.user.dto.BuyStocksResponse;
+import tech.ian.user.entity.*;
 import tech.ian.user.enums.TransactionalType;
-import tech.ian.user.repository.AccountInvestmentRepository;
-import tech.ian.user.repository.AccountRepository;
+import tech.ian.user.repository.*;
 
 import java.math.BigDecimal;
 
@@ -27,37 +27,115 @@ public class StockService {
     private AccountInvestmentRepository accountInvestmentRepository;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private AccountRepository accountRepository;
 
-    public BigDecimal buyStocks(Integer quantity, String stockName, AccountInvestment accountInvestment, TransactionalAccount transactionalAccount) throws Exception {
+    @Autowired
+    private StocksRepository stocksRepository;
+
+    @Autowired
+    private TransactionalRepository transactionalRepository;
+
+    public BuyStocksResponse buyStocks(String stockName, Integer quantity) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userMail = authentication.getName();
+
+        User user = userRepository.findUserEntityByEmail(userMail);
+        AccountInvestment accountInvestment = accountInvestmentRepository.findByAccount(user.getAccount())
+                .orElseThrow(() -> new Exception("AccountInvestment not found "));
+
         var response = searchStocks(stockName);
-        var result = response.results().get(0).regularMarketDayHigh();
+        var result = response.results().get(0).regularMarketPrice();
         var price = result.multiply(BigDecimal.valueOf(quantity));
-
-        var accountInvestmentOpt = this.accountInvestmentRepository.findByEmail(accountInvestment.getAccount().getEmail());
-        if (accountInvestmentOpt.isEmpty()) {
-            accountInvestmentOpt = this.accountInvestmentRepository.findByDocument(accountInvestment.getAccount().getDocument());
-            if (accountInvestmentOpt.isEmpty()) {
-                throw new Exception("Account not found");
-            }
-        }
-
-        accountInvestment = accountInvestmentOpt.get();
+        var dayPrice = result;
 
         if (accountInvestment.getAccount().getBalance().compareTo(price) < 0) {
             throw new Exception("Saldo insuficiente");
+        }
+
+        accountInvestment.getAccount().setBalance(accountInvestment.getAccount().getBalance().subtract(price));
+
+        accountInvestment.setStockBalance(accountInvestment.getStockBalance().add(price));
+
+        TransactionalAccount transactionalAccount = new TransactionalAccount();
+        transactionalAccount.setTransactionalType(TransactionalType.PURCHASE);
+        transactionalAccount.setAmount(price);
+
+        this.transactionalRepository.save(transactionalAccount);
+        this.accountRepository.save(accountInvestment.getAccount());
+        this.accountInvestmentRepository.save(accountInvestment);
+
+        var existingStockOpt = this.stocksRepository.findByStockId(stockName);
+
+        Stocks stocksEntity;
+
+        if (existingStockOpt.isEmpty()) {
+            stocksEntity = new Stocks();
+            stocksEntity.setStockId(stockName);
+            stocksEntity.setPrice(dayPrice);
+            stocksEntity.setQuantity(quantity);
+            stocksEntity.setTotalValue(dayPrice.multiply(BigDecimal.valueOf(quantity)));
+            stocksEntity.setAccountInvestment(accountInvestment);
+            stocksRepository.save(stocksEntity);
         } else {
-            accountInvestment.getAccount().setBalance(accountInvestment.getAccount().getBalance().subtract(price));
+            stocksEntity = existingStockOpt.get();
+            stocksEntity.setQuantity(stocksEntity.getQuantity() + quantity);
+            stocksEntity.setPrice(dayPrice);
+            stocksEntity.setTotalValue(stocksEntity.getTotalValue().add(dayPrice.multiply(BigDecimal.valueOf(quantity))));
+            stocksRepository.save(stocksEntity);
+        }
 
-            transactionalAccount.setTransactionalType(TransactionalType.PURCHASE);
 
-            accountInvestment.setStockBalance(accountInvestment.getStockBalance().add(price));
+        return new BuyStocksResponse(accountInvestment.getAccount().getBalance());
+    }
 
-            this.accountRepository.save(accountInvestment.getAccount());
+    public String sellStock(String stockName, Integer quantity) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userMail = authentication.getName();
+
+        User user = userRepository.findUserEntityByEmail(userMail);
+        AccountInvestment accountInvestment = accountInvestmentRepository.findByAccount(user.getAccount())
+                .orElseThrow(() -> new Exception("AccountInvestment not found "));
+
+        var response = searchStocks(stockName);
+        var result = response.results().get(0).regularMarketPrice();
+        var price = result.multiply(BigDecimal.valueOf(quantity));
+        var dayPrice = result;
+
+        var stock = this.stocksRepository.findByStockId(stockName);
+
+        Stocks stocksEntity;
+
+        if (stock.isEmpty()) {
+            throw new Exception("Acao nao encontrada");
+        } else {
+            stocksEntity = stock.get(); // Obtendo a entidade existente
+            stocksEntity.setQuantity(stocksEntity.getQuantity() - quantity);
+
+            if (stocksEntity.getTotalValue() == null) {
+                stocksEntity.setTotalValue(BigDecimal.ZERO);
+            }
+
+            stocksEntity.setTotalValue(stocksEntity.getTotalValue().subtract(dayPrice.multiply(BigDecimal.valueOf(quantity))));
+            this.stocksRepository.save(stocksEntity);
+
+            accountInvestment.setStockBalance(accountInvestment.getStockBalance().subtract(dayPrice.multiply(BigDecimal.valueOf(quantity))));
             this.accountInvestmentRepository.save(accountInvestment);
 
-            return accountInvestment.getStockBalance();
+            Account account = user.getAccount();
+            account.setBalance(account.getBalance().add(dayPrice.multiply(BigDecimal.valueOf(quantity))));
+            this.accountRepository.save(account);
+
+            TransactionalAccount transactionalAccount = new TransactionalAccount();
+            transactionalAccount.setTransactionalType(TransactionalType.SELL);
+            transactionalAccount.setAmount(price);
+
+            transactionalRepository.save(transactionalAccount);
         }
+
+        return "Venda Realizada com sucesso";
     }
 
     public ResponseBrapiDto searchStocks(String stockName) {
